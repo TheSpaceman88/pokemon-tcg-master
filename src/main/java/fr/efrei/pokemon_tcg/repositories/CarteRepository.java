8a5875a73a2b633b3d757a913ebb2912bbd3bc19
TheSpaceMan88 <alex.poissonneau@yahoo.fr>
2025-01-30 16:41:09 +0100

    modified:   src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java

--- src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java
+++ src/main/java/fr/efrei/pokemon_tcg/repositories/CarteRepository.java
@@ -1,9 +1,13 @@
 package fr.efrei.pokemon_tcg.repositories;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.efrei.pokemon_tcg.models.Carte;
+import fr.efrei.pokemon_tcg.models.Dresseur;
 
 @Repository
 public interface CarteRepository extends JpaRepository<Carte, Long> {
+  
 }

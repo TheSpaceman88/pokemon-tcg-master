405582c64e65770b6f2a5e2be8bfb0ad04257ff7
Alicequentrec <aquentrec.info@free.fr>
2025-01-31 15:17:13 +0100

    new file:   src/main/java/fr/efrei/pokemon_tcg/constants/PokemonConstants.java 	deleted:    src/main/java/fr/efrei/pokemon_tcg/constants/TypeCarte.java

---
+++ src/main/java/fr/efrei/pokemon_tcg/constants/PokemonConstants.java
@@ -0,0 +1,42 @@
+package fr.efrei.pokemon_tcg.constants;
+
+public class PokemonConstants {
+
+    public enum TypeCarte {
+        Commun,      // 0
+        Peu_commun,  // 1
+        Rare,        // 2
+        Epic,        // 3
+        Legendaire   // 4
+    }
+
+    public enum NomCarte {
+        Pikachu,     
+        Evoli,  
+        Dracaufeu,       
+        Mewtwo,        
+        Dracolosse,
+        Rayquaza,
+        Arceus
+    }
+
+    public enum Attaque1 {
+        Eclair,      
+        Morsure,     
+        Lance_Flammes, 
+        Psyko,       
+        Ouragan,     
+        Draco_Ascension, 
+        Jugement     
+    }
+
+    public enum Attaque2 {
+        Tonnerre,    
+        Vive_Attaque,
+        Deflagration, 
+        Ball_Ombre,  
+        Ultralaser,  
+        Colère,     
+        Punition    
+    }
+}
